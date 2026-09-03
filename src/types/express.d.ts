92f50d70633e0a9808e61798f191f40eacacda4d
Shahid Hasan Shovu <shahid@softvence.com>
2026-09-03 09:44:23 +0600

    fix(build): add types to dependencies and define explicit Express Request/User declarations

---
+++ src/types/express.d.ts
@@ -0,0 +1,13 @@
+declare global {
+  namespace Express {
+    interface User {
+      userId: string;
+      role: string;
+    }
+    interface Request {
+      user?: User;
+    }
+  }
+}
+
+export {};
